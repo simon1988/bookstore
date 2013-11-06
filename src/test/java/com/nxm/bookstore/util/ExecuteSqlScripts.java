5c6eec343820ccb5451b6734e0a6c018df8cd0a5
simon1988 <simon.niu1988@gmail.com>
2013-11-06 11:12:31 +0800

    add annotation solution for executing sql scripts before test cases

---
+++ src/test/java/com/nxm/bookstore/util/ExecuteSqlScripts.java
@@ -0,0 +1,20 @@
+package com.nxm.bookstore.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * This annotation aims to let you execute sql scripts before @Test methods.
+ * Will roll back the scripts if methods are annotated with @Transactional.
+ * 
+ * @author xn91913
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD) 
+public @interface ExecuteSqlScripts {
+	String[] value() default {};
+}
---
+++ src/test/java/com/nxm/bookstore/util/ExecuteSqlScriptsTestExecutionListener.java
@@ -0,0 +1,40 @@
+package com.nxm.bookstore.util;
+
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+import org.springframework.util.Assert;
+
+public class ExecuteSqlScriptsTestExecutionListener extends AbstractTestExecutionListener {
+	static Logger logger = Logger.getLogger(ExecuteSqlScriptsTestExecutionListener.class);
+	@Override
+	public void beforeTestMethod(TestContext testContext) throws Exception {
+		final Method testMethod = testContext.getTestMethod();
+		Assert.notNull(testMethod, "test method must not be null");
+		if(testMethod.isAnnotationPresent(ExecuteSqlScripts.class)){
+			ExecuteSqlScripts sqlScripts = testMethod.getAnnotation(ExecuteSqlScripts.class);
+			if(sqlScripts==null || sqlScripts.value()==null){
+				return;
+			}
+			//Autowire the signal datasource, assume test context contains only 1 datasource :h2
+			DataSource dataSource = testContext.getApplicationContext().getBean(DataSource.class);
+			Connection connection = DataSourceUtils.getConnection(dataSource);
+			ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+			for(String filename: sqlScripts.value()){
+				logger.debug("Prepare to execute " + filename + " before " + testMethod.getName());
+				populator.addScript(testContext.getApplicationContext().getResource(filename));
+			}
+			try {
+				populator.populate(connection);
+			} finally{
+				DataSourceUtils.releaseConnection(connection, dataSource);
+			}
+		}
+	}
+}
