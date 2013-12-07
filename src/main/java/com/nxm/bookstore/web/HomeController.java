8b2fe0456de26de6cf6328894b8cbab146cb41c5
simon1988 <simon.niu1988@gmail.com>
2013-12-07 17:22:07 +0800

    add jackson implementation to return json objects

--- src/main/java/com/nxm/bookstore/web/HomeController.java
+++ src/main/java/com/nxm/bookstore/web/HomeController.java
@@ -1,11 +1,14 @@
 package com.nxm.bookstore.web;
 
+import java.util.Collection;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nxm.bookstore.model.Order;
 import com.nxm.bookstore.service.BookService;
 
 @Controller
@@ -14,10 +17,15 @@ public class HomeController {
 	@Autowired
 	BookService bookservice;
 	
-	@RequestMapping("/")
+	@RequestMapping("/booklist")
 	public String showHomePage(Map<String,Object> model){
 		model.put("owner", "Mr Niu");
 		model.put("all_orders", bookservice.getAllOrders());
 		return "welcome";
+	}
+	
+	@RequestMapping("/ajax/getAllOrders.do")
+	public @ResponseBody Collection<Order> getAllOrders(){
+		return bookservice.getAllOrders();
 	}
 }
