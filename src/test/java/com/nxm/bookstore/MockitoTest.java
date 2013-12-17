8f7d352a3561c25811666bb00f0e94672dda4c80
simon1988 <simon.niu1988@gmail.com>
2013-12-17 21:15:17 +0800

    add mockito test

---
+++ src/test/java/com/nxm/bookstore/MockitoTest.java
@@ -0,0 +1,29 @@
+package com.nxm.bookstore;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MockitoTest {
+
+	@Test
+	public void testMock(){
+		List mockedList = mock(List.class);
+		
+		mockedList.add("one");
+		
+		when(mockedList.get(100)).thenReturn("100");
+		Assert.assertEquals("100", mockedList.get(100));
+		
+		verify(mockedList).add("one");
+//		verify(mockedList).add("two");
+	}
+}
