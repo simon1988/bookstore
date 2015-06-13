1766e709db2f90a73d4aeb7fa428da1e4a968f29
niuxingman <niuxingman@corp.netease.com>
2015-06-13 20:50:19 +0800

    Add utils, put HTML5 doctype to jsps

---
+++ src/main/java/com/nxm/bookstore/util/JsonUtils.java
@@ -0,0 +1,46 @@
+package com.nxm.bookstore.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JsonUtils {
+	private static Logger logger=Logger.getLogger(JsonUtils.class);
+	private static ObjectMapper mapper = new ObjectMapper();
+	
+	public static String objectToJson(Object object) {
+		try {
+			return mapper.writeValueAsString(object);
+		} catch (Exception e) {
+			logger.warn("write to json string error:" + object, e);
+			return null;
+		}
+	}
+	
+	public static <T> T jsonToObject(String jsonString, Class<T> clazz) {
+		if (StringUtils.isEmpty(jsonString)) {
+			return null;
+		}
+		try {
+			return mapper.readValue(jsonString, clazz);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("parse json string error:" + jsonString, e);
+			return null;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String args[]){
+		Map<String,Object> map = new HashMap<>();
+		map.put("retCode", 200);
+		map.put("ret", "test");
+		String json = objectToJson(map);
+		System.out.println(json);
+		map = jsonToObject(json,Map.class);
+		System.out.println(map.get("retCode"));
+	}
+}
