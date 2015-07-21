3d31d4ccd2987635a9f256fb1c157142d7b1667e
niuxingman <niuxingman@corp.netease.com>
2015-07-21 21:57:00 +0800

    Add upload file

--- src/main/java/com/nxm/bookstore/web/HomeController.java
+++ src/main/java/com/nxm/bookstore/web/HomeController.java
@@ -1,5 +1,8 @@
 package com.nxm.bookstore.web;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.servlet.http.Cookie;
@@ -15,7 +18,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nxm.bookstore.model.Book;
 import com.nxm.bookstore.service.BookService;
@@ -154,5 +159,24 @@ public class HomeController {
 	public byte[] qrcode() throws IOException {
 		byte[] qrpic = qrService.generateQRImage("http://www.baidu.com");
 	    return qrpic;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/upload")
+	public String upload(@RequestParam("name") String name, @RequestParam("file") MultipartFile file) {
+		logger.info("upload "+name+"!");
+		if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + "!";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
 	}
 }
